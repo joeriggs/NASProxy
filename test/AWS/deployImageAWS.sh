fc3fcf0ba3b0e996566b8854a5b856c9614d408c
jriggs <joe.riggs.1962@gmail.com>
2020-07-12 15:32:11 -0400

    Deploy to AWS updates.
    It still doesn't do everything, but there are notes in the file that explain a lot of the stuff that needs to be automated.  So the steps are mostly there.
    
    Currently the end result would be that the OVA file is imported and turned into an AWS AMI.  There aren't steps to "Launch" it yet.  We'll worry about that after we get it to the point where we can automate the process of uploading and importing the NASProxy.ova to an AWS AMI.

--- test/AWS/deployImageAWS.sh
+++ test/AWS/deployImageAWS.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+readonly BLD_DIR=$( cd `dirname ${0}`    && echo ${PWD} )
+readonly TOP_DIR=$( cd ${BLD_DIR}/../..  && echo ${PWD} )
+
+readonly OVA_FILE_NAME=NASProxy.ova
+readonly OVA_FILE_PATH=${TOP_DIR}/${OVA_FILE_NAME}
+
 ####################
 # Create and initialize a log file.
 readonly LOG=/tmp/`basename ${0}`.log
@@ -18,6 +24,25 @@ echo    "IAM configuration:"
 echo -n "  Check ... "
 aws configure get aws_access_key_id &> ${LOG}
 [ $? -ne 0 ] && echo "Fail." && exit 1 ; echo "Pass."
+
+echo -n "  Create vmimport role ... "
+aws iam create-role --role-name vmimport --assume-role-policy-document "file:///home/jriggs/NASProxy/test/AWS/trust-policy.json" &> ${LOG}
+if [ $? -ne 0 ]; then
+	grep -q "Role with name vmimport already exists." ${LOG}
+	if [ $? -eq 0 ]; then
+		echo "Already exists."
+	else
+		echo "Fail."
+		exit 1
+	fi
+else
+	echo "Pass."
+fi
+
+echo -n "  Put role policy ... "
+aws iam put-role-policy --role-name vmimport --policy-name vmimport --policy-document "file:///home/jriggs/NASProxy/test/AWS/role-policy.json" &> ${LOG}
+[ $? -ne 0 ] && echo "Fail." && exit 1 ; echo "Pass."
+
 echo ""
 
 ####################
@@ -35,34 +60,54 @@ else
 fi
 
 echo -n "  Upload ... "
-aws s3 cp NASProxy.ova s3://nas.proxy/ &> ${LOG}
+aws s3 cp ${OVA_FILE_PATH} s3://nas.proxy/ &> ${LOG}
 [ $? -ne 0 ] && echo "Fail." && exit 1 ; echo "Pass."
 
 echo ""
-
-exit 0
 
 ####################
 # Import the OVA file into AWS.
-echo -n "  Upload ... "
-aws s3 cp NASProxy.ova s3://nas.proxy/ &> ${LOG}
-aws ec2 import-image --description "NASProxy VM" --disk-containers "file://C:\import\containers.json"
-[ $? -ne 0 ] && echo "Fail." && exit 1 ; echo "Pass."
+##### echo -n "  Import ... "
+##### aws ec2 import-image --description "NASProxy VM" --disk-containers "file://C:\import\containers.json" &> ${LOG}
+##### [ $? -ne 0 ] && echo "Fail." && exit 1 ; echo "Pass."
 
-echo ""
+# NOTE THE OUTPUT FROM THE PREVIOUS COMMAND WILL INCLUDE THE FOLLOWING.
+#
+#     "ImportTaskId": "import-ami-0fdee75f7e8c152e4",
+#
+# ImportTaskId IS THE ID THAT YOU PASS TO THE "aws ec2 describe-import-image-tasks"
+#              COMMANDS (SHOWN BELOW).
+
+##### echo ""
+
+# The following command will report the status of the "aws ec2 import-image"
+# command that we just executed.  You can sit in a loop and run this command
+# occasionally (maybe once per minute, or 30 seconds) to track the progress.
+
+##### aws ec2 describe-import-image-tasks --import-task-ids import-ami-0fdee75f7e8c152e4
+
+# Search the output from the command for one of the following:
+# "StatusMessage" indicates the command is still running.  This is the status
+#                 of the import operation.
+#
+# "SnapshotId": "snap-0d9caed7044176ce5" appears when the command is finished.
+#                 There will no longer be a "StatusMessage" message.  You can
+#                 scan your list of AMIs and see an image with this name.
 
 ####################
 # Done.  Cleanup.
 echo    "Cleanup:"
 echo -n "  Delete OVA file ... "
-aws s3 rm s3://nas.proxy/NASProxy.ova
+aws s3 rm s3://nas.proxy/${OVA_FILE_NAME} &> ${LOG}
 [ $? -ne 0 ] && echo "Fail." && exit 1 ; echo "Pass."
 
 echo -n "  Remove bucket ... "
-aws s3 rb s3://nas.proxy
+aws s3 rb s3://nas.proxy &> ${LOG}
 [ $? -ne 0 ] && echo "Fail." && exit 1 ; echo "Pass."
+
 echo ""
 
+####################
 echo "Success."
 exit 0
 
